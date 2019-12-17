f3aee8d0e8b72b75c6c48df2d4f6fa712e0e4867
Max Zakzook <mzakzook@me.com>
2019-12-16 23:31:12 -0800

    Reset password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   # default url option
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -52,4 +52,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
 end
